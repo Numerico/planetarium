af91702a68c7cfb2485e29c6d4654d8c1de3caca
Numerico <webmaster@numerica.cl>
2013-10-24 14:37:52 -0300

    feature passing with simple http response

--- planetarium/features/home.feature
+++ planetarium/features/home.feature
@@ -2,4 +2,4 @@ Feature: Be able to run cucumber tests for a django app
 
   Scenario: Visit home page
     Given I browse to the home page
-      Then I should see a h1 that says "DTurno.cl"
+      Then I should see a h1 that says "Planetarium"
--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from planetarium import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^$', views.index, name='index')
     # Examples:
     # url(r'^$', 'planetarium.views.home', name='home'),
     # url(r'^planetarium/', include('planetarium.foo.urls')),
