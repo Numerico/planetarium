702113f4d7766778b1d2d579f5467c6a51096770
Numerico <webmaster@numerica.cl>
2013-10-24 14:10:15 -0300

    cucumber working

--- planetarium/features/support/env.rb
+++ planetarium/features/support/env.rb
@@ -1,25 +1,24 @@
-# https://github.com/jwmayfield/django-cucumber-demo/commit/86ee47e5f9f30c5ae9dc1130a686567d7ca806cf
 require 'capybara/cucumber'
+require 'capybara-webkit'
 
-Capybara.app = "planetarium"
 Capybara.run_server = false
-Capybara.default_driver = :selenium
+Capybara.default_driver = :webkit
 Capybara.app_host = "http://localhost:12345"
+Capybara.automatic_reload = false
 
-#World do
-#  @virtualenv_path = File.expand_path(File.dirname(__FILE__) + "/../../../")
-#  `python #{@virtualenv_path}/manage.py testserver --addrport 12345 #{@virtualenv_path}/planetarium/fixtures/test_admin_user.json > #{@virtualenv_path}/tmp/testrun.log 2>&1 &`
-  # extend Capybara
-#end
+World do
+  @virtualenv_path = File.expand_path(File.dirname(__FILE__) + "/../../../")
+  `python #{@virtualenv_path}/manage.py testserver --addrport 12345 #{@virtualenv_path}/planetarium/fixtures/test_admin_user.json > #{@virtualenv_path}/tmp/testrun.log 2>&1 &`
+end
 
-# Before do
-#  begin
-#    visit('/')
-#  rescue Selenium::WebDriver::Error::WebDriverError
-#    retry
-#  end
-# end
+Before do
+  begin
+    visit('/')
+  rescue Capybara::Webkit::InvalidResponseError
+    retry
+  end
+end
 
-#at_exit do
-#  `ps -ef | grep '.*manage.py testserver .*[: ]*12345' | grep -v grep | awk '{print $2}' | xargs kill -9`
-#end
+at_exit do
+  `ps -ef | grep '.*manage.py testserver .*[: ]*12345' | grep -v grep | awk '{print $2}' | xargs kill -9`
+end
